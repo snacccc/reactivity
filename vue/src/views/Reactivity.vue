1d58e9cba73a9b514bdb51002f9265e46bb124d5
snacccc <80086013+snacccc@users.noreply.github.com>
2022-11-14 13:13:17 -0500

    decided on topic

--- vue/src/views/Reactivity.vue
+++ vue/src/views/Reactivity.vue
@@ -4,40 +4,40 @@
       
       <div class="row one">
 
-        <Button @click="add7" class="numpad">7</Button>
-        <Button @click="add8" class="numpad">8</Button>
-        <Button @click="add9" class="numpad">9</Button>
+        <Button @click="addMT" class="input mt">Milk Tea</Button>
+        <Button @click="addGT" class="input gt">Green Tea</Button>
+        <Button @click="addBT" class="input bt">Black Tea</Button>
 
       </div>
 
       <div class="row two">
 
-        <Button @click="add4" class="numpad">4</Button>
-        <Button @click="add5" class="numpad">5</Button>
-        <Button @click="add6" class="numpad">6</Button>
+        <Button @click="" class="input">4</Button>
+        <Button @click="" class="input">5</Button>
+        <Button @click="" class="input">6</Button>
 
       </div>
 
       <div class="row three">
 
-        <Button @click="add1" class="numpad">1</Button>
-        <Button @click="add2" class="numpad">2</Button>
-        <Button @click="add3" class="numpad">3</Button>
+        <Button @click="" class="input">1</Button>
+        <Button @click="" class="input">2</Button>
+        <Button @click="" class="input">3</Button>
 
       </div>
 
       <div class="row four">
 
-        <Button @click="clearNums" class="numpad clear">Clear</Button>
-        <Button @click="add0" class="numpad">0</Button>
-        <Button @click="enterNums" class="numpad enter">Enter</Button>
+        <Button @click="clearChoices" class="input clear">Clear</Button>
+        <Button @click="" class="input">Add Another</Button>
+        <Button @click="enterChoices" class="input enter">Enter</Button>
 
       </div>
 
     </div>
 
     <div class="output">
-        <header v-for="number in numbers"> {{ number.value }}</header>
+        <header v-for="choice in choices"> {{ choice }}</header>
       </div>
 
   </div>
@@ -54,45 +54,24 @@
     },
     data() {
       return {
-        numbers: [],
+        choices: [],
       }
     },
     methods: {
-      add0 () {
-        this.numbers.push({value: '0'})
+      addMT () {
+        this.choices.push('Milk Tea')
       },
-      add1 () {
-        this.numbers.push({value: '1'})
+      addGT () {
+        this.choices.push('Green Tea')
       },
-      add2 () {
-        this.numbers.push({value: '2'})
+      addBT () {
+        this.choices.push('Black Tea')
       },
-      add3 () {
-        this.numbers.push({value: '3'})
+      clearChoices () {
+        this.choices.splice(0);
       },
-      add4 () {
-        this.numbers.push({value: '4'})
-      },
-      add5 () {
-        this.numbers.push({value: '5'})
-      },
-      add6 () {
-        this.numbers.push({value: '6'})
-      },
-      add7 () {
-        this.numbers.push({value: '7'})
-      },
-      add8 () {
-        this.numbers.push({value: '8'})
-      },
-      add9 () {
-        this.numbers.push({value: '9'})
-      },
-      clearNums () {
-        this.numbers.splice(0);
-      },
-      enterNums () {
-        console.log(this.numbers)
+      enterChoices () {
+        console.log(this.choices)
       },
     }
   }
@@ -102,7 +81,7 @@
 <style scoped>
   .reactivity {
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     margin: 1rem;
   }
 
@@ -110,31 +89,39 @@
     margin: 0.5rem;
   }
 
-  .numpad {
+  .input {
     margin: 0.5rem;
+    width: 5.5rem;
+    height: 4.5rem;
+    box-shadow: 0px 10px gray;
+  }
+
+  .input:active {
+    transform: translateY(5px);
+    box-shadow: 0px 5px gray
   }
 
   .enter {
     background-color: var(--enterprimary);
-    box-shadow: 0px 8px var(--entershadow);
+    box-shadow: 0px 10px var(--entershadow);
   }
 
   .enter:active {
-    box-shadow: 0px 4px var(--entershadow);
+    box-shadow: 0px 5px var(--entershadow);
   }
 
   .clear {
     background-color: var(--clearprimary);
-    box-shadow: 0px 8px var(--clearshadow);
+    box-shadow: 0px 10px var(--clearshadow);
   }
 
   .clear:active {
-    box-shadow: 0px 4px var(--clearshadow);
+    box-shadow: 0px 5px var(--clearshadow);
   }
 
   .output {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     font-size: 1.5rem;
     color: white;
     margin: 1rem;
