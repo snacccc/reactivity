5ddf4a6e9d48c62c493a01ff7fc690f8bf2eb7fa
snacccc <80086013+snacccc@users.noreply.github.com>
2022-11-30 13:13:34 -0500

    made code easier to understand

--- vue/src/views/Reactivity.vue
+++ vue/src/views/Reactivity.vue
@@ -4,58 +4,60 @@
       
       <div class="row one">
 
-        
-        <Button v-if="gtChosen && btChosen && entered" @click="addItem('Milk Tea'); mtChosen = !mtChosen" class="input">Milk Tea</Button>
+        <Button v-if="!gtChosen && !btChosen && !entered" @click="addItem('Milk Tea'); mtChosen = !mtChosen" class="input">Milk Tea</Button>
         <Button v-else class="input" disabled>Milk Tea</Button>
-        <Button v-if="mtChosen && btChosen && entered" @click="addItem('Green Tea'); gtChosen = !gtChosen" class="input">Green Tea</Button>
+        <Button v-if="!mtChosen && !btChosen && !entered" @click="addItem('Green Tea'); gtChosen = !gtChosen" class="input">Green Tea</Button>
         <Button v-else class="input" disabled>Green Tea</Button>
-        <Button v-if="mtChosen && gtChosen && entered" @click="addItem('Black Tea'); btChosen = !btChosen" class="input">Black Tea</Button>
+        <Button v-if="!mtChosen && !gtChosen && !entered" @click="addItem('Black Tea'); btChosen = !btChosen" class="input">Black Tea</Button>
         <Button v-else class="input" disabled>Black Tea</Button>
 
       </div>
 
       <div class="row two">
 
-        <Button v-if="hiChosen && liChosen && entered" @click="addItem('No Ice'); niChosen = !niChosen" class="input">No Ice</Button>
+        <Button v-if="!hiChosen && !liChosen && !entered" @click="addItem('No Ice'); niChosen = !niChosen" class="input">No Ice</Button>
         <Button v-else class="input" disabled>No Ice</Button>
-        <Button v-if="niChosen && liChosen && entered" @click="addItem('Half Ice'); hiChosen = !hiChosen" class="input">Half Ice</Button>
+        <Button v-if="!niChosen && !liChosen && !entered" @click="addItem('Half Ice'); hiChosen = !hiChosen" class="input">Half Ice</Button>
         <Button v-else class="input" disabled>Half Ice</Button>
-        <Button v-if="niChosen && hiChosen && entered" @click="addItem('Less Ice'); liChosen = !liChosen" class="input">Less Ice</Button>
+        <Button v-if="!niChosen && !hiChosen && !entered" @click="addItem('Less Ice'); liChosen = !liChosen" class="input">Less Ice</Button>
         <Button v-else class="input" disabled>Less Ice</Button>
 
       </div>
 
       <div class="row three">
 
-        <Button v-if="lsChosen && esChosen && entered" @click="addItem('50% Sugar'); hsChosen = !hsChosen" class="input">50% Sugar</Button>
+        <Button v-if="!lsChosen && !esChosen && !entered" @click="addItem('50% Sugar'); hsChosen = !hsChosen" class="input">50% Sugar</Button>
         <Button v-else class="input" disabled>50% Sugar</Button>
-        <Button v-if="hsChosen && esChosen && entered" @click="addItem('75% Sugar'); lsChosen = !lsChosen" class="input">75% Sugar</Button>
+        <Button v-if="!hsChosen && !esChosen && !entered" @click="addItem('75% Sugar'); lsChosen = !lsChosen" class="input">75% Sugar</Button>
         <Button v-else class="input" disabled>75% Sugar</Button>
-        <Button v-if="hsChosen && lsChosen && entered" @click="addItem('125% Sugar'); esChosen = !esChosen" class="input">125% Sugar</Button>
+        <Button v-if="!hsChosen && !lsChosen && !entered" @click="addItem('125% Sugar'); esChosen = !esChosen" class="input">125% Sugar</Button>
         <Button v-else class="input" disabled>125% Sugar</Button>
 
       </div>
 
       <div class="row four">
 
-        <Button v-if="entered" @click="clearChoices" class="input clear">Clear</Button> 
-        <Button v-else-if="!confirmed" class="input clear" disabled>Cancel</Button>
+        <Button v-if="!entered" @click="clearChoices" class="input clear">Clear</Button> 
+        <Button v-else-if="confirmed" class="input clear" disabled>Cancel</Button> 
         <Button v-else class="input clear" @click="entered = !entered">Cancel</Button> 
-        <Button v-if="entered" @click="addItem('Tapioca')" class="input">Tapioca</Button>
+        <Button v-if="!entered" @click="addItem('Tapioca')" class="input">Tapioca</Button>
         <Button v-else class="input" disabled>Tapioca</Button>
-        <Button v-if="entered" @click="entered = !entered" class="input enter">Enter</Button>
-        <Button v-else-if="!entered && confirmed" class="input enter" @click="addItem('Order Confirmed!'); confirmed = !confirmed">Confirm Order</Button>
-        <Button v-if="!confirmed" class="input enter" @click="clearChoices">Order Again</Button>
+        <Button v-if="(mtChosen && !entered) || (gtChosen && !entered) || (btChosen && !entered)" @click="entered = !entered" class="input enter">Enter</Button>
+        <Button v-else-if="entered && !confirmed" class="input enter" @click="addItem('Order Confirmed!'); confirmed = !confirmed">Confirm Order</Button>
+        <Button v-else-if="!confirmed" class="input enter" disabled>Enter</Button>
+        <Button v-if="confirmed" class="input enter" @click="clearChoices">Order Again</Button>
 
       </div>
 
     </div>
 
     <div class="output">
-        <header v-for="choice in choices"> {{ choice }}</header>
+        <header v-for="choice in choices"> {{ choice }} </header>
     </div>
 
-    <img src="../img/" class="image">
+    <img src="../img/milktea.png" v-if="(confirmed && mtChosen)" class="image">
+    <img src="../img/greentea.png" v-if="(confirmed && gtChosen)" class="image">
+    <img src="../img/blacktea.png" v-if="(confirmed && btChosen)" class="image">
 
   </div>
 </template>
@@ -72,17 +74,17 @@
     data() {
       return {
         choices: [],
-        mtChosen: true,
-        gtChosen: true,
-        btChosen: true,
-        niChosen: true,
-        hiChosen: true,
-        liChosen: true,
-        hsChosen: true,
-        lsChosen: true,
-        esChosen: true,
-        entered: true,
-        confirmed: true,
+        mtChosen: false,
+        gtChosen: false,
+        btChosen: false,
+        niChosen: false,
+        hiChosen: false,
+        liChosen: false,
+        hsChosen: false,
+        lsChosen: false,
+        esChosen: false,
+        entered: false,
+        confirmed: false,
       }
     },
     methods: {
@@ -96,17 +98,17 @@
       },
       clearChoices () {
         this.choices.splice(0);
-        this.mtChosen = true;
-        this.gtChosen = true;
-        this.btChosen = true;
-        this.niChosen = true;
-        this.hiChosen = true;
-        this.liChosen = true;
-        this.hsChosen = true;
-        this.lsChosen = true;
-        this.esChosen = true;
-        this.entered = true;
-        this.confirmed = true;
+        this.mtChosen = false;
+        this.gtChosen = false;
+        this.btChosen = false;
+        this.niChosen = false;
+        this.hiChosen = false;
+        this.liChosen = false;
+        this.hsChosen = false;
+        this.lsChosen = false;
+        this.esChosen = false;
+        this.entered = false;
+        this.confirmed = false;
       },
     }
   }
